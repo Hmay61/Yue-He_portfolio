9e67f6c0a2463031690306815086160831fb6e2f
YUE HE <may61he@gmail.com>
2016-09-03 22:37:28 -0400

    Update my.js

--- my.js
+++ my.js
@@ -1,3 +1,25 @@
-	$(document).ready(function(){
-		$('[data-toggle="tooltip"]').tooltip();
-	});
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();
+
+	//foucus name, give hint
+	$("#name").bind({"focus":function(){
+		$("#nameMsg").html("Please input your name");
+	},"blur":function(){
+		$("#nameMsg").html("");
+	}});
+
+	//foucus email, give hint
+	$("#email").bind({"focus":function(){
+		$("#emailMsg").html("Please input your email");
+	},"blur":function(){
+		$("#emailMsg").html("");
+	}});
+
+	//foucus message, give hint
+	$("#message").bind({"focus":function(){
+		$("#textMsg").html("You can give me some comments");
+	},"blur":function(){
+		$("#textMsg").html("");
+	}});
+	
+});
